d7c06e22d24aaa33d39359f899cfadb5f2186935
James W <51789232+FalsePozitive@users.noreply.github.com>
2023-07-06 15:44:52 +0200

    initialized
    next onto chapter 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from "react";
 
 const IndexPage = () => {
-  return <main />;
+  return <main> 
+    <h1>this is home</h1>
+  </main>;
 };
 
 export default IndexPage;
---
+++ src/pages/test.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return <main> 
+    <h1>this is test
+
+    </h1>
+  </main>;
+};
+
+export default IndexPage;
+
+export const Head = () => (
+<>
+<meta content="this is the page descripton" name="description"></meta>
+<title>test Page</title>
+</>);
